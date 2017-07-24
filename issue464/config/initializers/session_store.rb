88fb50fcaa5ff2607c5a5a3e9b2ea98d4b0794bd
meganemura <meganemura@users.noreply.github.com>
2017-07-24 23:58:48 +0900

    $ git grep -l 456 | xargs sed -i "" -e"s/456/464/g"
    Using BSD sed

--- issue464/config/initializers/session_store.rb
+++ issue464/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_issue456_session'
+Rails.application.config.session_store :cookie_store, key: '_issue464_session'
